b88a909f50d83c6106d47af70f8cfdac6fc37af5
Simon Nelson <simo96i1@edu.zealand.dk>
2022-06-14 10:15:52 +0200

    Opgave 1

--- Badetidssystemet/Kreds.cs
+++ Badetidssystemet/Kreds.cs
@@ -6,5 +6,29 @@ namespace Badetidssystemet
 {
     class Kreds
     {
+        public Kreds()
+        {
+
+        }
+
+        public void NewKreds(int ID, string Navn, string Adresse, int AntalDeltagere) 
+        {
+            newID = ID;
+            newNavn = Navn;
+            newAdresse = Adresse;
+            newAntalDeltagere = AntalDeltagere;
+
+            Console.WriteLine("ID  : - " + newID + "Navn  : - " + newNavn + "Adresse  : - " + newAdresse + "Antal Deltagere : - " + newAntalDeltagere);
+            Console.ReadLine();
+
+
+        }
+        int newID;
+        string newNavn;
+        string newAdresse;
+        int newAntalDeltagere;
+
+
+
     }
 }
--- Badetidssystemet/Program.cs
+++ Badetidssystemet/Program.cs
@@ -2,11 +2,30 @@
 
 namespace Badetidssystemet
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+class Program
+{
+       
+       static void Main(string[] args)
+       {
+        int ID;
+        string Navn;
+        string Adresse;
+        int AntalDeltagere;
+
+
+        Console.WriteLine("Indtast følgende oplysninger: ID, navn, Adresse og antal deltagere");
+        ID = Convert.ToInt32(Console.ReadLine());
+        Navn = Console.ReadLine();
+        Adresse = Console.ReadLine();
+        AntalDeltagere = Convert.ToInt32(Console.ReadLine());
+
+
+        Kreds newKreds = new Kreds();
+        newKreds.NewKreds(ID, Navn, Adresse, AntalDeltagere);
+
+
+
+
+       }
+}
 }
